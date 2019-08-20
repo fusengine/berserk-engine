248f9a00f0002dfc8fd804bcc43fb39da2430b73
Bruno Azoulay <azoulaybruno@gmail.com>
2019-08-20 11:16:34 +0200

    expressjs: add port listen

---
+++ README.md
@@ -0,0 +1,3 @@
+![alt Berserk](./logo.png "Berserk Engine")
+
+Berserk-engine use [express](https://github.com/expressjs/express) to create simple framework.
---
+++ index.js
@@ -0,0 +1,13 @@
+/*!
+ * berserEngine
+ * Copyright(c) 2019 Bruno Azoulay
+ * Copyright(c) 2019 Fusengine
+ * MIT Licensed
+ */
+
+// const path = require('path')
+// const pathBerserk = path.resolve(__dirname, 'config.js')
+
+module.exports = {
+	berserkEngine: require('./lib')(),
+}
