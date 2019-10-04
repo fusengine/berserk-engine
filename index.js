74f5f863615e40aa415bd36ab6b80c2477b2c708
Bruno Azoulay <azoulaybruno@gmail.com>
2019-10-04 11:35:43 +0200

    add cookie-parser && fix engine

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const configTest = path.resolve(__dirname, 'config.js')
 
 console.log(process.env.NODE_ENV)
 module.exports = {
-	engine: require('./lib')(configTest),
+	engine: require('./lib'),
 	berserkPath: require('./lib/params/utils').Path,
 }
