713b9d52663248de8a67282e5b67329d281137a6
Bruno Azoulay <info@fusengine.ch>
2020-04-12 15:29:14 +0200

    features(upgrade, add): Upgrade package and add clear

--- index.js
+++ index.js
@@ -7,12 +7,17 @@
  */
 const Utils = require('./lib/utils')
 const config = require('./config')
+const clear = require('clear')
+
+/** Clear terminal */
+clear()
 
 /** Message */
 Utils.successMessage(`Berserk: engine loaded mode ${Utils.ENV()} \n `)
 
 if (Utils.ENV() === 'test') {
 	module.exports = require('./lib/berserk').engine(config)
-} else {
+}
+else {
 	module.exports = require('./lib/berserk')
 }
